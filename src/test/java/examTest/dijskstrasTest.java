927060520c626da108b668130904d34efd84e8a5
Julen Lazcano <julenlazkano9@gmail.com>
2022-01-28 10:26:55 +0000

    trece

--- src/test/java/examTest/dijskstrasTest.java
+++ src/test/java/examTest/dijskstrasTest.java
@@ -1,11 +1,20 @@
 package examTest;
-
+import com.thealgorithms.datastructures.graphs.*;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
 
 import org.junit.jupiter.api.Test;
 
 class dijskstrasTest {
 
 	
+	
+	@DisplayName("Hola")
+	@Test
+	void test() {
+		int j =3;
+		assertEquals(3,j);
+	}
 
 }
